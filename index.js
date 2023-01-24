5824587cffa69b2416baab09bf72994fd52f4cab
Sachtony <24889366+sachtony@users.noreply.github.com>
2023-01-24 12:10:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 app.controller('myPrice', function ($scope, $http) {
+    $scope.Servers = "1.1.1.1";
     $scope.Coin = "SHIB";
     $scope.Price = "0.1";    
     $http.get("index.html").then(function (response) {
