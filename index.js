124756bc72d84b115ac527683bd7f81ee0f00652
SHT Network <24889366+sachtony@users.noreply.github.com>
2025-06-10 10:15:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 app.controller('myPrice', function ($scope, $http) {
     $scope.Servers = "1.1.1.1";
-    $scope.Coin = "SHIB";
+    $scope.Coin = "SHT";
     $scope.Price = "0.1";    
     $http.get("index.html").then(function (response) {
         $scope.index = response.data;
